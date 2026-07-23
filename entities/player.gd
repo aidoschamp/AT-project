2f191e4b2f2e4513558049ef228a71c1715f6c5e
Aiden Brunner <aidenzoebrunner@gmail.com>
2026-07-23 19:28:10 +1000

    fixed being able to shoot in menu

--- entities/player.gd
+++ entities/player.gd
@@ -65,55 +65,56 @@ func _physics_process(delta: float) -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("jarvis mode"):
-		jarvis_mode_timer.start()
-	if event.is_action_released("jarvis mode"):
-		jarvis_mode_timer.stop()
+	if not get_tree().paused:
+		if event.is_action_pressed("jarvis mode"):
+			jarvis_mode_timer.start()
+		if event.is_action_released("jarvis mode"):
+			jarvis_mode_timer.stop()
 
 
-	if event.is_action_pressed("place mine") and inventory[Enums.Items.MINE] > 0:
-		inventory[Enums.Items.MINE] -= 1
-		var mine: DistractionMine = MINE_SCENE.instantiate() # create mine
-		mine.position = position
-		mines.add_child(mine)
-		enemy.add_target(mine)
+		if event.is_action_pressed("place mine") and inventory[Enums.Items.MINE] > 0:
+			inventory[Enums.Items.MINE] -= 1
+			var mine: DistractionMine = MINE_SCENE.instantiate() # create mine
+			mine.position = position
+			mines.add_child(mine)
+			enemy.add_target(mine)
 
-	# shoot only if gun is reloaded
-	if event.is_action_pressed("lidar shoot") and gun_timer.time_left == 0:
-		gun_timer.start()
-		for i in range(DOT_COUNT):
-			var dot: Dot = DOT_SCENE.instantiate() # create dot
-			var angle: Vector2 = get_global_mouse_position() - global_position # set base angle and fix for player rotation
-			angle = angle.rotated(current_spread * (i / (DOT_COUNT - 1)) - current_spread / 2) # position angle so that each dot is evenly spread
-			dot.position = position
-			dot.angle = angle.normalized()
-			lidars.add_child(dot)
-	
-	
-	if event.is_action_pressed("shotgun shoot") and inventory[Enums.Items.SHOTGUN] > 0:
-		for i in range(DOT_COUNT * 2):
-			inventory[Enums.Items.SHOTGUN] -= 1
-			var dot: Dot = DOT_SCENE.instantiate() # create dot
-			var angle: Vector2 = get_global_mouse_position() - global_position # set base angle and fix for player rotation
-			angle = angle.rotated(PI / 3 * (i / (DOT_COUNT * 2 - 1)) - PI / 3 / 2) # position angle so that each dot is evenly spread
-			dot.position = position
-			dot.angle = angle.normalized()
-			dot.is_shotgun_bullet = true
-			lidars.add_child(dot)
-	
-	
-	var spread_change_direction := Input.get_axis("decrease spread", "increase spread")
-	
-	# change spread of dots if not trying to change out of spread bounds
-	if current_spread + spread_change_amount < max_spread and spread_change_direction == 1 or current_spread - spread_change_amount > PI / 3 and spread_change_direction == -1:
-		current_spread += spread_change_amount * spread_change_direction
-		queue_redraw()
-	elif spread_change_direction == 1:
-		current_spread = max_spread
-		queue_redraw()
-	elif spread_change_direction == -1:
-		current_spread = PI / 3
-		queue_redraw()
+		# shoot only if gun is reloaded
+		if event.is_action_pressed("lidar shoot") and gun_timer.time_left == 0:
+			gun_timer.start()
+			for i in range(DOT_COUNT):
+				var dot: Dot = DOT_SCENE.instantiate() # create dot
+				var angle: Vector2 = get_global_mouse_position() - global_position # set base angle and fix for player rotation
+				angle = angle.rotated(current_spread * (i / (DOT_COUNT - 1)) - current_spread / 2) # position angle so that each dot is evenly spread
+				dot.position = position
+				dot.angle = angle.normalized()
+				lidars.add_child(dot)
+		
+		
+		if event.is_action_pressed("shotgun shoot") and inventory[Enums.Items.SHOTGUN] > 0:
+			for i in range(DOT_COUNT * 2):
+				inventory[Enums.Items.SHOTGUN] -= 1
+				var dot: Dot = DOT_SCENE.instantiate() # create dot
+				var angle: Vector2 = get_global_mouse_position() - global_position # set base angle and fix for player rotation
+				angle = angle.rotated(PI / 3 * (i / (DOT_COUNT * 2 - 1)) - PI / 3 / 2) # position angle so that each dot is evenly spread
+				dot.position = position
+				dot.angle = angle.normalized()
+				dot.is_shotgun_bullet = true
+				lidars.add_child(dot)
+		
+		
+		var spread_change_direction := Input.get_axis("decrease spread", "increase spread")
+		
+		# change spread of dots if not trying to change out of spread bounds
+		if current_spread + spread_change_amount < max_spread and spread_change_direction == 1 or current_spread - spread_change_amount > PI / 3 and spread_change_direction == -1:
+			current_spread += spread_change_amount * spread_change_direction
+			queue_redraw()
+		elif spread_change_direction == 1:
+			current_spread = max_spread
+			queue_redraw()
+		elif spread_change_direction == -1:
+			current_spread = PI / 3
+			queue_redraw()
 
 
 func _on_jarvis_mode_timer_timeout() -> void:
