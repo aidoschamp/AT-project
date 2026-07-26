fb85c8dcff6d5c2b3e58b0045beb5a61178e1af4
Aiden Brunner <aidenzoebrunner@gmail.com>
2026-07-26 19:26:09 +1000

    added a wall break sfx

--- floors/floor_2_break_wall.gd
+++ floors/floor_2_break_wall.gd
@@ -2,9 +2,12 @@ extends Area2D
 
 
 @onready var walls: TileMapLayer = $"../../TileMaps/TileMapLayer"
+@onready var wall_break_sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 
 func _on_body_entered(_body: Node2D) -> void:
+	wall_break_sound.play()
+
 	for cell in range(7):
 		walls.erase_cell(Vector2i(-63, -95 + cell))
 
